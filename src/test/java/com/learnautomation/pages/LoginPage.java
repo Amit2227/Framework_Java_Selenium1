36de473b0945acc425b45bbacfae18b437d535d1
ashrivastav <ashrivastav@xento.com>
2022-03-28 22:44:42 +0530

    second commit

--- src/test/java/com/learnautomation/pages/LoginPage.java
+++ src/test/java/com/learnautomation/pages/LoginPage.java
@@ -5,15 +5,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
-	
-	WebDriver driver;
-	
-	public LoginPage(WebDriver ldriver)
-	{
-		this.driver=ldriver;
-		
-	}
-	
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver ldriver) {
+        this.driver = ldriver;
+
+    }
+
 //	@FindBy(name="login") WebElement user;
 //	
 //	@FindBy(name="passwd") WebElement pass;
@@ -21,36 +20,35 @@ public class LoginPage {
 //	@FindBy(xpath="//input[@class='signinbtn']") WebElement loginButton;
 //	
 //	@FindBy(xpath="//a[contains(text(),'Signout')]") WebElement signoutButton;
-	
-	@FindBy(name="email") WebElement user;
-	
-	@FindBy(name="password") WebElement pass;
-	
-	@FindBy(xpath="//span[contains(text(),'Login')]") WebElement loginButton;
-	
-	//@FindBy(xpath="//a[contains(text(),'Signout')]") WebElement signoutButton;
-	
 
-	public void loginCRM(String usernameApplication, String passwordApplication)
-	{
-		try {
-			
-			Thread.sleep(2000);
-			
-		}
-		catch(InterruptedException e){
-			
-		}
-		
-		user.clear();
-		user.sendKeys(usernameApplication);
-		pass.clear();
-		pass.sendKeys(passwordApplication);
-		loginButton.click();
-		//signoutButton.click();
-	}
+    @FindBy(name = "email")
+    WebElement user;
+
+    @FindBy(name = "password")
+    WebElement pass;
+
+    @FindBy(xpath = "//span[contains(text(),'Login')]")
+    WebElement loginButton;
+
+    //@FindBy(xpath="//a[contains(text(),'Signout')]") WebElement signoutButton;
 
 
+    public void loginCRM(String usernameApplication, String passwordApplication) {
+        try {
 
-	
+            Thread.sleep(2000);
+
+        } catch (InterruptedException e) {
+
+        }
+
+        user.clear();
+        user.sendKeys(usernameApplication);
+        pass.clear();
+        pass.sendKeys(passwordApplication);
+        loginButton.click();
+        //signoutButton.click();
+    }
+
+
 }
--- src/test/java/com/learnautomation/testcases/LoginTestCRM.java
+++ src/test/java/com/learnautomation/testcases/LoginTestCRM.java
@@ -10,32 +10,30 @@ import com.learnautomation.utilities.Helper;
 
 
 public class LoginTestCRM extends BaseClass {
-	
-		
-	@Test(priority=1)
-	public void loginApp()
-	{
-		
-		logger=report.createTest("Login to PHP Travels");
-		
-		logger.info("Start Application");
-		
-		LoginPage loginpage= PageFactory.initElements(driver, LoginPage.class);
-		loginpage.loginCRM(excel.getStringData("Login", 0, 0),excel.getStringData("Login", 0, 1));
-		
-		logger.pass("Login Success");
-					
-			
-	}
-	
-	@Test(priority=2)
-	public void logOut()
-	{
-		report.createTest("Logout");
-		logger.fail("Logout Fail");
-		
-		//System.out.println("LogOut Sucessfully");
-			
-	}
+
+
+    @Test(priority = 1)
+    public void loginApp() {
+
+        logger = report.createTest("Login to PHP Travels");
+
+        logger.info("Start Application");
+
+        LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
+        loginpage.loginCRM(excel.getStringData("Login", 0, 0), excel.getStringData("Login", 0, 1));
+
+        logger.pass("Login Success");
+
+
+    }
+
+    @Test(priority = 2)
+    public void logOut() {
+        report.createTest("Logout");
+        logger.fail("Logout Fail");
+
+        //System.out.println("LogOut Sucessfully");
+
+    }
 
 }
